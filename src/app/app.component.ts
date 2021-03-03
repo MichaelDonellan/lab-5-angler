dbca0a676e5764894dbc64206f3224f7e2e5776c
Michael <you@example.commichaeldonellan1999@gmail.com>
2021-03-03 15:23:52 +0000

    lab5 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-data-binding-app';
+
+
+  numberOfTimes:number = 0;
+hide:boolean = true;
+
+  onButtonClick(){
+  this.numberOfTimes++;
+
+  }
+
+  onStarClick(){
+if(this.hide ==true){
+ this.hide = false; 
+}
+
+else{
+  this.hide=true
+}
+
+
+  }
 }
